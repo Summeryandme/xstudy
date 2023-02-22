d2c8631e8e030d13407b9053f3d9dd9c8cc3d0c5
Mingwei Sun <mingwei.sun@thoughtworks.com>
2023-02-22 21:15:32 +0800

    feature: integrated with nacos.

--- xstudy-content/src/main/java/com/xstudy/content/XstudyContentApplication.java
+++ xstudy-content/src/main/java/com/xstudy/content/XstudyContentApplication.java
@@ -3,6 +3,7 @@ package com.xstudy.content;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @EnableSwagger2Doc
 @SpringBootApplication(scanBasePackages = "com.xstudy")
 public class XstudyContentApplication {
